80943a8710f5019f498c40eeee82dad499c277c3
timhuijsmans1 <timhuijsmans@hotmail.com>
2022-10-02 21:04:40 +0100

    remove tweet_printer file

--- auto_labelling/individual_steps/data_collector/tweet_printer.py
+++
@@ -1,6 +0,0 @@
-import json
-with open("./collected_data/filtered_search_results_07-18-2022_11;15;20.txt", 'r') as f:
-	for tweet in f:
-		text = json.loads(tweet)['text']
-		print(text)
-		print('-' * 30)
